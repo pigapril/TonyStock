63af9966e5d3128bfb7568350a2470230b9aada4
TonyHuang <pigapril@gmail.com>
2025-07-21 00:18:52 +0800

    自動偵測語言

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector'; // 移除或註解掉
-// import Backend from 'i18next-http-backend'; // 不再需要
+import LanguageDetector from 'i18next-browser-languagedetector'; // 新增
 
 // 直接 import json 檔案
 import en from './locales/en/translation.json';
@@ -9,8 +8,7 @@ import zhTW from './locales/zh-TW/translation.json';
 
 // 初始化 i18n，直接用 resources
 i18n
-  // .use(Backend) // 不再需要
-  // .use(LanguageDetector) // 如需自動偵測語言可再啟用
+  .use(LanguageDetector) // 啟用自動偵測
   .use(initReactI18next)
   .init({
     fallbackLng: {
