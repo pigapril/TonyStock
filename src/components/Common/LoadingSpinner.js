024adfeef9ebcee6d6f6994faf4fc36e75f402c0
TonyHuang <pigapril@gmail.com>
2025-08-26 22:16:17 +0800

    fixed laoding spinner

--- src/components/Common/LoadingSpinner.js
+++ src/components/Common/LoadingSpinner.js
@@ -7,13 +7,13 @@ import React from 'react';
 const LoadingSpinner = ({ size = 'medium', className = '' }) => {
     const sizeClasses = {
         small: 'w-4 h-4',
-        medium: 'w-8 h-8',
-        large: 'w-12 h-12'
+        medium: 'w-6 h-6',
+        large: 'w-8 h-8'
     };
 
     return (
-        <div className={`flex justify-center items-center ${className}`}>
-            <div className={`${sizeClasses[size]} animate-spin`}>
+        <div className={`inline-flex items-center gap-2 ${className}`}>
+            <div className={`${sizeClasses[size]} animate-spin flex-shrink-0`}>
                 <svg
                     className="w-full h-full text-blue-600"
                     fill="none"
