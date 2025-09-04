931d6137a022bd4fdd06660f974f00c10ef0ed67
TonyHuang <pigapril@gmail.com>
2025-09-04 23:37:28 +0800

    fix URL language variation

--- src/components/FloatingSponsorButton/FloatingSponsorButton.js
+++ src/components/FloatingSponsorButton/FloatingSponsorButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import './FloatingSponsorButton.css'; // 確保導入 CSS 樣式
 import sponsorIcon from '../../assets/images/sponsor-us/sponsor_icon.gif'; // 將副檔名改為 .gif
 
 const FloatingSponsorButton = () => {
   const { i18n } = useTranslation();
+  const { lang } = useParams();
   const currentLanguage = i18n.language;
 
   if (currentLanguage !== 'zh-TW') {
@@ -12,7 +14,7 @@ const FloatingSponsorButton = () => {
   }
 
   return (
-    <a href="/sponsor-us" className="floating-sponsor-button">
+    <a href={`/${lang}/sponsor-us`} className="floating-sponsor-button">
       <img src={sponsorIcon} alt="贊助我們" />
     </a>
   );
